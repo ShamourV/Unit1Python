c6a38b5b3c14fd1c3ddea3a72fc38eac2e9886d0
ShamourV <svassell2@brooklynsteamcenter.org>
2023-09-19 09:50:39 -0400

    GG

---
+++ benchmark.py
@@ -0,0 +1,32 @@
+# Exercise 1: Display name in console
+
+print; "Shamour"
+
+# Exercise 2: Create a variable for favroite show
+# And set it equal to your favorite show
+
+FavoriteShow = "Dragon Ball"
+
+# Exercise 3: Create two variable called height and age then display in the console, then display in the console.
+
+Y = "5'11"
+Age = "17"
+
+
+# Exercise 4: Display "Im going to break" in the console
+
+print; "im going to break" 
+
+# Exercise 5: List of favroite food
+
+list; 'Rice, goat, chicken, and lamb'
+
+# Excersise 6: create conditional statement if age is greater than or equal to 21
+if Age > 21: print("You can rent a car")
+
+# Excersie 7: Ask user for their name then display in the console, "Hello their name"
+
+# Excersise 8: Create a loop that will display each item from fav food on a seperate line on the console
+
+# Excersise 9: Create a while loop that will never run
+While: 
