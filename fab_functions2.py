134576df435f81ec1848898b94adbe9a68c1f00a
ShamourV <svassell2@brooklynsteamcenter.org>
2023-10-13 10:53:58 -0400

    Cel

---
+++ fab_functions2.py
@@ -0,0 +1,31 @@
+"""
+Task 1: Calculate the Square of a Number
+Write a function that takes an integer as an argument and returns its square.
+"""
+def square(int):
+    return  int**2
+est=square(5)
+print(est)
+"""
+Task 2: Calculate the Area of a Rectangle:
+Write a function that takes the length and width of a rectangle and returns its area.
+"""
+def aor(length, width):
+    return length * width
+drink = aor(8,8)
+print(drink)
+
+"""
+Task 3: Convert Temperature from Celsius to Fahrenheit:
+Write a function that takes a temperature in Celsius and returns 
+the equivalent temperature in Fahrenheit using the correct formula
+"""
+
+def celci(C):
+    return F = C *(5/2)+ 32
+convert = celci(10)
+"""
+Task 4: Calculate the Average of Numbers:
+Write a function that takes a list of numbers 
+and returns the average (mean) of those numbers.
+"""
